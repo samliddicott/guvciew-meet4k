83f8f42c9b1db53ddfa22febfb6b4ff7a0f54924
Paulo Assis <pj.assis@gmail.com>
2008-10-27 19:17:22 +0000

    * forgot to add the files again, must be Parkison setting in

---
+++ src/timers.c
@@ -0,0 +1,92 @@
+/*******************************************************************************#
+#	    guvcview              http://guvcview.berlios.de                    #
+#                                                                               #
+#           Paulo Assis <pj.assis@gmail.com>                                    #
+#										#
+# This program is free software; you can redistribute it and/or modify         	#
+# it under the terms of the GNU General Public License as published by   	#
+# the Free Software Foundation; either version 2 of the License, or           	#
+# (at your option) any later version.                                          	#
+#                                                                              	#
+# This program is distributed in the hope that it will be useful,              	#
+# but WITHOUT ANY WARRANTY; without even the implied warranty of             	#
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  		#
+# GNU General Public License for more details.                                 	#
+#                                                                              	#
+# You should have received a copy of the GNU General Public License           	#
+# along with this program; if not, write to the Free Software                  	#
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA	#
+#                                                                              	#
+********************************************************************************/
+
+#include "timers.h"
+
+/* called by capture from start timer [-t seconds] command line option*/
+gboolean
+timer_callback(gpointer data)
+{
+	struct ALL_DATA * all_data = (struct ALL_DATA *) data;
+	struct GLOBAL *global = all_data->global;
+	struct GWIDGET *gwidget = all_data->gwidget;
+	
+	/*stop avi capture*/
+	capture_avi(GTK_BUTTON(gwidget->CapAVIButt), &all_data);
+	global->Capture_time=0; 
+	return (FALSE);/*destroys the timer*/
+}
+
+/*called by timed capture [-c seconds] command line option*/
+gboolean
+Image_capture_timer(gpointer data)
+{
+	struct ALL_DATA * all_data = (struct ALL_DATA *) data;
+	struct GLOBAL *global = all_data->global;
+	struct GWIDGET *gwidget = all_data->gwidget;
+	struct vdIn *videoIn = all_data->videoIn; 
+	
+	/*increment image name (max 1-99999)*/
+	int sfname=strlen(global->imgFPath[0]);
+	char basename[sfname];
+	char extension[4];
+	sscanf(global->imgFPath[0],"%[^.].%3c",basename,extension);
+	int namesize=strlen(global->imgFPath[1])+strlen(basename)+5;
+	
+	extension[3] = '\0';
+	
+	if(namesize>110) {
+		videoIn->ImageFName=realloc(videoIn->ImageFName,namesize+11);
+	}
+	
+	sprintf(videoIn->ImageFName,"%s/%s-%i.%s",global->imgFPath[1],
+			                        basename,global->image_inc,extension );
+	snprintf(global->imageinc_str,24,_("File num:%d"),global->image_inc);
+		
+	gtk_label_set_text(GTK_LABEL(gwidget->ImageIncLabel), global->imageinc_str);
+	
+	global->image_inc++;
+	/*set image capture flag*/
+	videoIn->capImage = TRUE;
+	if(global->image_inc > global->image_npics) {/*destroy timer*/
+		gtk_button_set_label(GTK_BUTTON(gwidget->CapImageButt),_("Cap. Image"));
+		global->image_timer=0;
+		set_sensitive_img_contrls(TRUE, gwidget);/*enable image controls*/
+		return (FALSE);
+	}
+	else return (TRUE);/*keep the timer*/
+}
+
+/* called by fps counter every 2 sec */
+gboolean 
+FpsCount_callback(gpointer data)
+{
+	struct ALL_DATA * all_data = (struct ALL_DATA *) data;
+	struct GLOBAL *global = all_data->global;
+	
+	global->DispFps = (double) global->frmCount / 2;
+	if (global->FpsCount>0) return(TRUE); /*keeps the timer*/
+	else {
+		snprintf(global->WVcaption,10,"GUVCVideo");
+		SDL_WM_SetCaption(global->WVcaption, NULL);
+		return (FALSE);/*destroys the timer*/
+	}
+}
---
+++ src/timers.h
@@ -0,0 +1,54 @@
+/*******************************************************************************#
+#	    guvcview              http://guvcview.berlios.de                    #
+#                                                                               #
+#           Paulo Assis <pj.assis@gmail.com>                                    #
+#										#
+# This program is free software; you can redistribute it and/or modify         	#
+# it under the terms of the GNU General Public License as published by   	#
+# the Free Software Foundation; either version 2 of the License, or           	#
+# (at your option) any later version.                                          	#
+#                                                                              	#
+# This program is distributed in the hope that it will be useful,              	#
+# but WITHOUT ANY WARRANTY; without even the implied warranty of             	#
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  		#
+# GNU General Public License for more details.                                 	#
+#                                                                              	#
+# You should have received a copy of the GNU General Public License           	#
+# along with this program; if not, write to the Free Software                  	#
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA	#
+#                                                                              	#
+********************************************************************************/
+
+#ifndef TIMERS_H
+#define TIMERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <SDL/SDL.h>
+#include <signal.h>
+#include <X11/Xlib.h>
+#include <SDL/SDL_syswm.h>
+/* support for internationalization - i18n */
+#include <glib/gi18n.h>
+#include "../config.h"
+
+#include <gtk/gtk.h>
+#include <gdk/gdkx.h>
+
+#include "v4l2uvc.h"
+#include "globals.h"
+#include "guvcview.h"
+#include "callbacks.h"
+
+gboolean
+timer_callback(gpointer data);
+
+gboolean
+Image_capture_timer(gpointer data);
+
+gboolean 
+FpsCount_callback(gpointer data);
+
+#endif
