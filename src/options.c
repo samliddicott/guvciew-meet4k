785e023db1391e6e4c2518a1a7d1b75f68fed286
Paulo Assis <pj.assis@gmail.com>
2008-11-25 09:46:24 +0000

    * parse command line arguments with getopt

--- src/options.c
+++ src/options.c
@@ -24,6 +24,8 @@
 #include <string.h>
 #include <linux/videodev.h>
 #include <sys/file.h>
+#include <unistd.h>
+#include <getopt.h>
 /* support for internationalization - i18n */
 #include <glib/gi18n.h>
 
@@ -53,7 +55,7 @@ writeConf(struct GLOBAL *global) {
 		fprintf(fp,"vpane=%i\n",global->boxvsize);
 		fprintf(fp,"#spin button behavior: 0-non editable 1-editable\n");
 		fprintf(fp,"spinbehave=%i\n", global->spinbehave);
-		fprintf(fp,"# mode video format 'uyv' 'yuv' 'yup' or 'jpg'(default)\n");
+		fprintf(fp,"# mode video format 'yuv' 'uyv' 'yup' 'gbr' or 'jpg'(default)\n");
 		fprintf(fp,"mode=%s\n",global->mode);
 		fprintf(fp,"# frames per sec. - hardware supported - default( %i )\n",DEFAULT_FPS);
 		fprintf(fp,"fps=%d/%d\n",global->fps_num,global->fps);
@@ -239,161 +241,170 @@ readConf(struct GLOBAL *global) {
 void
 readOpts(int argc,char *argv[], struct GLOBAL *global) {
 	
-	int i=0;
-	char *separateur;
-	
-	for (i = 1; i < argc; i++) {
-	
-		/* skip bad arguments */
-		if (argv[i] == NULL || *argv[i] == 0 || *argv[i] != '-') {
-			continue;
-		}
-	    	if (strcmp(argv[i], "--verbose") == 0) {
-			global->debug=1; /*debug mode*/
-		}
-	    	
-		if ((strcmp(argv[i], "-d") == 0) || (strcmp(argv[i], "--device") == 0)) {
-			if (i + 1 >= argc || *argv[i+1] =='-') {
-				printf("No parameter specified for device, using default.\n");
-			} else {
-				snprintf(global->videodevice,15,"%s",argv[i + 1]);
-			}
-		}
-		if (strcmp(argv[i], "-g") == 0) {
-			/* Ask for read instead default  mmap */
-			global->grabmethod = 0;
-		}
-		if (strcmp(argv[i], "-w") == 0) {
-			if ( i + 1 >= argc || *argv[i+1] =='-') {
-				printf("No parameter specified with -w, using default.\n");	
-			} else {
-				if (strcmp(argv[i+1], "enable") == 0) global->hwaccel=1;
-				else 
-					if (strcmp(argv[i+1], "disable") == 0) global->hwaccel=0;
-			}
-		}
-		if ((strcmp(argv[i], "-f") == 0) || (strcmp(argv[i], "--format") == 0)) {
-			if ( i + 1 >= argc || *argv[i+1] =='-') {
-				printf("No parameter specified for format, using default.\n");	
-			} else {
-				global->mode[0] = argv[i + 1][0];
-				global->mode[1] = argv[i + 1][1];
-				global->mode[2] = argv[i + 1][2];
-			}
-		}
-		if ((strcmp(argv[i], "-s") == 0) || (strcmp(argv[i], "--size") == 0)) {
-			if (i + 1 >= argc || *argv[i+1] =='-') {
-			printf("No parameter specified for image size, using default.\n");
-			} else {
-				char *sizestring = NULL;
-				sizestring = strdup(argv[i + 1]);
+    int c = 0;
+    static int debug_flag = 0;
+    char *tmpstr = NULL;
+    char *separateur;
+    
+    static struct option long_options[] = {
+          /* These options set a flag. */
+          {"verbose", no_argument, &debug_flag, 1},
+          {"brief",   no_argument,  &debug_flag, 0},
+          /* These options don't set a flag.
+             We distinguish them by their indices. */
+          {"help",      no_argument,	   0, 'h'},
+          {"device",    required_argument, 0, 'd'},
+          {"hwd_acel",  required_argument, 0, 'w'},
+          {"format",    required_argument, 0, 'f'},
+          {"size",      required_argument, 0, 's'},
+	  {"image",     required_argument, 0, 'i'},
+	  {"cap_time",  required_argument, 0, 'c'},
+	  {"npics",     required_argument, 0, 'm'},
+	  {"avi",       required_argument, 0, 'n'},
+	  {"avi_time",  required_argument, 0, 't'},
+	  {"show_fps",  required_argument, 0, 'p'},
+	  {"profile",   required_argument, 0, 'l'},
+          {0, 0, 0, 0}
+    };
+      /* getopt_long stores the option index here. */
+    int option_index = 0;
+    
+    
+    while (c >= 0)
+    {
+      c = getopt_long (argc, argv, "hd:w:f:s:i:c:m:n:t:p:l:",
+                       long_options, &option_index);
 
-				global->width = strtoul(sizestring, &separateur, 10);
-				if (*separateur != 'x') {
-					printf("Error in size usage: -s[--size] widthxheight \n");
-				} else {
-					++separateur;
-					global->height = strtoul(separateur, &separateur, 10);
-					if (*separateur != 0)
-						printf("hmm.. dont like that!! trying this height \n");
-				}
-				free(sizestring);
-			}
-			printf(" size width: %d height: %d \n",global->width, global->height);
-		}
-		if ((strcmp(argv[i], "-c") == 0) || (strcmp(argv[i], "--captime") == 0)) {
-			if (i + 1 >= argc || *argv[i+1] =='-') {
-				printf("No parameter specified for image capture time. Ignoring option.\n");	
-			} else {
-				char *timestr = strdup(argv[i + 1]);
-				global->image_timer= strtoul(timestr, &separateur, 10);
-				global->image_inc=1;
-				printf("capturing images every %i seconds",global->image_timer);
-				free(timestr);
-			}
-		}
-		if ((strcmp(argv[i], "-m") == 0) || (strcmp(argv[i], "--npics") == 0)) {
-			if (i + 1 >= argc || *argv[i+1] =='-') {
-				printf("No parameter specified for number of pics. Ignoring option.\n");	
-			} else {
-				char *npicstr = strdup(argv[i + 1]);
-				global->image_npics= strtoul(npicstr, &separateur, 10);
-				printf("capturing at max %d pics",global->image_npics);
-				free(npicstr);
-			}
-		}
-		if ((strcmp(argv[i], "-i") == 0) || (strcmp(argv[i], "--image") == 0)) {
-			if (i + 1 >= argc || *argv[i+1] =='-') {
-				printf("No parameter specified for image name. Ignoring option.\n");	
-			} else {
-				char *image_path = strdup(argv[i + 1]);
-				global->imgFPath=splitPath(image_path,global->imgFPath);
-				/*get the file type*/
-				global->imgFormat = check_image_type(global->imgFPath[0]);
-				free(image_path);
-			}
-		}
-		
-		if ((strcmp(argv[i], "-n") == 0) || (strcmp(argv[i], "--avi") == 0)) {
-			if (i + 1 >= argc || *argv[i+1] =='-') {
-				printf("No parameter specified for avi name. Ignoring option.\n");	
-			} else {
-				global->avifile = strdup(argv[i + 1]);
+      /* Detect the end of the options. */
+      if (c == -1)
+        break;
+
+      switch (c)
+        {
+        case 0:
+          /* If this option set a flag, do nothing else now. */
+          if (long_options[option_index].flag != 0)
+            break;
+          printf ("option %s", long_options[option_index].name);
+          if (optarg)
+            printf (" with arg %s", optarg);
+          printf ("\n");
+          break;
+
+        case 'd':
+  	    snprintf(global->videodevice,15,"%s",optarg);
+  	    break;
+
+        case 'w':
+  	    if (strcmp(optarg, "enable") == 0) global->hwaccel=1;
+	    else 
+	    if (strcmp(optarg, "disable") == 0) global->hwaccel=0;
+  	    break;
+
+        case 'f':
+	    snprintf(global->mode,4,"%s",optarg);
+  	    //global->mode[0] = optarg[0];
+  	    break;
+
+        case 's':
+	    tmpstr = strdup(optarg);
+	    
+	    global->width = strtoul(tmpstr, &separateur, 10);
+	    if (*separateur != 'x') {
+		printf("Error in size usage: -s[--size] widthxheight \n");
+	    } else {global->avifile = strdup(optarg);
 				global->aviFPath=splitPath(global->avifile,global->aviFPath);
-			}
-		}
-		if ((strcmp(argv[i], "-t") == 0) || (strcmp(argv[i], "--avitime") == 0)) {
-			if (i + 1 >= argc || *argv[i+1] =='-') {
-				printf("No parameter specified for avi time. Ignoring option.\n");	
-			} else {
-				char *timestr = strdup(argv[i + 1]);
-				global->Capture_time= strtoul(timestr, &separateur, 10);
-				printf("capturing avi for %i seconds",global->Capture_time);
-				free(timestr);
-			}
-		}
-		if (strcmp(argv[i], "-p") == 0) {
-			if ( i + 1 >= argc || *argv[i+1] =='-') {
-				printf("No parameter specified with -p, using default.\n");	
-			} else {
-				if (strcmp(argv[i+1], "enable") == 0) global->FpsCount=1;
-				else 
-					if (strcmp(argv[i+1], "disable") == 0) global->FpsCount=0;
-			}
-		}
-		if ((strcmp(argv[i], "-l") == 0) || (strcmp(argv[i], "--profile") == 0)) {
-			if (i + 1 >= argc || *argv[i+1] =='-') {
-				printf("No parameter specified for profile name. Ignoring option.\n");	
-			} else {
-				global->lprofile=1;
-				global->profile_FPath=splitPath(argv[i + 1],global->profile_FPath);
-			}
-		}
-		if ((strcmp(argv[i], "-h") == 0) || (strcmp(argv[i], "--help") == 0)) {
-			printf("usage: guvcview [options] \n\n");
-			printf("options:\n");
-			printf("-h[--help]\t:print this message \n");
-		    	printf("--verbose \tverbose mode, prints a lot of debug related info\n");
-			printf("-d[--device] /dev/videoX\t:use videoX device\n");
-			printf("-g\t:use read method for grab instead mmap\n");
-			printf("-w enable|disable\t:SDL hardware accel. \n");
-			printf("-f[--format] format\t:video format\n");
-			printf("   default jpg  others options are uyv yuv yup jpg \n");
-			printf("-s[--size] widthxheight\t:use specified input size \n");
-			printf("-i[--image] image_file_name\t:sets the default image name\n"); 
-			printf("   available image formats: jpg png bmp\n");
-			printf("-c[--captime] time_in_seconds\t:time between image captures (sec.)\n"); 
-			printf("   enables auto image capture\n");
-			printf("-m[--npics] num_pics\t:max number of image captures\n");
-			printf("   defaults to 999 if not set\n");
-			printf("-n[--avi] avi_file_name\t:if set, enable avi capture from start \n");
-			printf("-t[--avitime] capture_time\t:used with -n option, avi capture time (sec.)\n");
-			printf("-p enable|disable\t:fps counter in title bar\n");
-			printf("-l[--profile] filename\t:loads the given control profile\n");
-			closeGlobals(global);
-			exit(0);
-		}
-	}
+		++separateur;
+		global->height = strtoul(separateur, &separateur, 10);
+		if (*separateur != 0)
+			printf("hmm.. dont like that!! trying this height \n");
+	    }
+	    free(tmpstr);
+	    tmpstr=NULL;
+  	    break;
+		
+	case 'i':
+	    tmpstr = strdup(optarg);
+	    global->imgFPath=splitPath(tmpstr,global->imgFPath);
+	    /*get the file type*/
+	    global->imgFormat = check_image_type(global->imgFPath[0]);
+	    free(tmpstr);
+	    tmpstr=NULL;
+	    break;
+		
+	case 'c':
+	    tmpstr = strdup(optarg);
+	    global->image_timer= strtoul(tmpstr, &separateur, 10);
+	    global->image_inc=1;
+	    printf("capturing images every %i seconds",global->image_timer);
+	    free(tmpstr);
+	    tmpstr=NULL;
+	    break;
+		
+	case 'm':
+	    tmpstr = strdup(optarg);
+	    global->image_npics= strtoul(tmpstr, &separateur, 10);
+	    printf("capturing at max %d pics",global->image_npics);
+	    free(tmpstr);
+	    tmpstr=NULL;
+	    break;
+	
+	case 'n':
+	    global->avifile = strdup(optarg);
+	    global->aviFPath=splitPath(global->avifile,global->aviFPath);
+	    break;
+		
+	case 't':
+	    tmpstr = strdup(optarg);
+	    global->Capture_time= strtoul(tmpstr, &separateur, 10);
+	    printf("capturing avi for %i seconds",global->Capture_time);
+	    free(tmpstr);
+	    tmpstr=NULL;
+	    break;
+		
+	case 'p':
+	    if (strcmp(optarg, "enable") == 0) global->FpsCount=1;
+	    else 
+	    if (strcmp(optarg, "disable") == 0) global->FpsCount=0;
+	    break;
+		
+	case 'l':
+	    global->lprofile=1;
+	    global->profile_FPath=splitPath(optarg,global->profile_FPath);
+	    break;
+		
+        case '?':
+          /* getopt_long already printed an error message. */
+          break;
+
+	case 'h':
+	default:
+	    printf("usage: guvcview [options] \n\n");
+	    printf("options:\n");
+	    printf("-h[--help]\t:print this message \n");
+	    printf("--verbose \tverbose mode, prints a lot of debug related info\n");
+	    printf("-d[--device] /dev/videoX\t:use videoX device\n");
+	    //printf("-g\t:use read method for grab instead mmap\n");
+	    printf("-w enable|disable\t:SDL hardware accel. \n");
+	    printf("-f[--format] format\t:video format\n");
+	    printf("   default jpg  others options are yuv uyv yup gbr jpg \n");
+	    printf("-s[--size] widthxheight\t:use specified input size \n");
+	    printf("-i[--image] image_file_name\t:sets the default image name\n"); 
+	    printf("   available image formats: jpg png bmp\n");
+	    printf("-c[--captime] time_in_seconds\t:time between image captures (sec.)\n"); 
+	    printf("   enables auto image capture\n");
+	    printf("-m[--npics] num_pics\t:max number of image captures\n");
+	    printf("   defaults to 999 if not set\n");
+	    printf("-n[--avi] avi_file_name\t:if set, enable avi capture from start \n");
+	    printf("-t[--avitime] capture_time\t:used with -n option, avi capture time (sec.)\n");
+	    printf("-p enable|disable\t:fps counter in title bar\n");
+	    printf("-l[--profile] filename\t:loads the given control profile\n");
+	    closeGlobals(global);
+  	    exit (0);
+	    	    
+        }
+    }
+	global->debug=debug_flag;
 	
 	/*if -n not set reset capture time*/
 	if(global->Capture_time>0 && global->avifile==NULL) global->Capture_time=0;
@@ -406,6 +417,9 @@ readOpts(int argc,char *argv[], struct GLOBAL *global) {
 		global->formind = 1;
 	} else if (strncmp(global->mode, "yup", 3) == 0) {
 		global->format = V4L2_PIX_FMT_YUV420;
+		global->formind = 1;
+	} else if (strncmp(global->mode, "gbr", 3) == 0) {
+		global->format = V4L2_PIX_FMT_SGBRG8;
 		global->formind = 1;
 	} else if (strncmp(global->mode, "jpg", 3) == 0) {
 		global->format = V4L2_PIX_FMT_MJPEG;
