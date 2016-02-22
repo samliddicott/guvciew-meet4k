48ddde96b304bf1f6a7c97f82bc3d8b0e7acc1b3
Paulo Assis <pj.assis@gmail.com>
2016-02-22 09:33:59 +0000

    make sure we can build against older kernels that do not support Y16_BE

--- gview_v4l2core/frame_decoder.c
+++ gview_v4l2core/frame_decoder.c
@@ -185,7 +185,9 @@ int alloc_v4l2_frames(v4l2_dev_t *vd)
 		case V4L2_PIX_FMT_GREY:
 		case V4L2_PIX_FMT_Y10BPACK:
 	    case V4L2_PIX_FMT_Y16:
+#ifdef V4L2_PIX_FMT_Y16_BE
 		case V4L2_PIX_FMT_Y16_BE:
+#endif
 			framebuf_size = framesizeIn;
 			/*frame queue*/
 			for(i=0; i<vd->frame_queue_size; ++i)
@@ -893,11 +895,11 @@ int decode_v4l2_frame(v4l2_dev_t *vd, v4l2_frame_buff_t *frame)
 	    case V4L2_PIX_FMT_Y16:
 			y16_to_yu12(frame->yuv_frame, frame->raw_frame, width, height);
 			break;
-
+#ifdef V4L2_PIX_FMT_Y16_BE
 		case V4L2_PIX_FMT_Y16_BE:
 			y16x_to_yu12(frame->yuv_frame, frame->raw_frame, width, height);
 			break;
-
+#endif
 		case V4L2_PIX_FMT_SPCA501:
 			s501_to_yu12(frame->yuv_frame, frame->raw_frame, width, height);
 			break;
