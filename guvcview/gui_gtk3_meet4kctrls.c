cffac68ab0eaac9ff7ac9a27e98a1d9e350e9353
Sam Liddicott <sam@liddicott.com>
2024-12-19 13:51:07 +0000

    Add background mode and blur level

--- guvcview/gui_gtk3_meet4kctrls.c
+++ guvcview/gui_gtk3_meet4kctrls.c
@@ -52,9 +52,10 @@ extern int is_control_panel;
  */
 GtkWidget *BackgroundMode = NULL;
 GtkWidget *CameraAngle = NULL;
+GtkWidget *Background = NULL;
 
 /*
- * meet4K background mode mode callback
+ * meet4K effect callback
  * args:
  *   combo - widget that caused the event
  *   data  - user data
@@ -64,15 +65,14 @@ GtkWidget *CameraAngle = NULL;
  *
  * returns: none
  */
-void meet4k_background_mode_changed(GtkComboBox *combo, void *data)
+void meet4k_camera_effect_changed(GtkComboBox *combo, void *data)
 {
-	uint8_t background_mode = (uint8_t) (gtk_combo_box_get_active (combo));
-
-	meet4kcore_set_background_mode(get_v4l2_device_handler(), background_mode);
+	uint8_t camera_effect = (uint8_t) (gtk_combo_box_get_active (combo));
+	meet4kcore_set_camera_effect(get_v4l2_device_handler(), camera_effect);
 }
 
 /*
- * meet4K background mode mode callback
+ * meet4K camera angle mode callback
  * args:
  *   combo - widget that caused the event
  *   data  - user data
@@ -87,6 +87,33 @@ void meet4k_camera_angle_changed(GtkComboBox *combo, void *data)
 	uint8_t camera_angle = (uint8_t) (gtk_combo_box_get_active (combo));
 
 	meet4kcore_set_camera_angle(get_v4l2_device_handler(), camera_angle);
+}
+
+/*
+ * meet4K background mode mode callback
+ * args:
+ *   combo - widget that caused the event
+ *   data  - user data
+ *
+ * asserts:
+ *   none
+ *
+ * returns: none
+ */
+void meet4k_background_changed(GtkComboBox *combo, void *data)
+{
+	uint8_t bg_mode = *(gtk_combo_box_get_active_id(combo));
+	meet4kcore_set_bg_mode(get_v4l2_device_handler(), bg_mode);
+    if (1 != gtk_combo_box_get_active(GTK_COMBO_BOX(BackgroundMode)))
+        gtk_combo_box_set_active(GTK_COMBO_BOX(BackgroundMode), 1);
+}
+
+void meet4k_blur_level_changed(GtkRange *range, void *data)
+{
+	uint8_t blur = (uint8_t) gtk_range_get_value (range);
+	meet4kcore_set_blur_level(get_v4l2_device_handler(), blur);
+    if (2 != gtk_combo_box_get_active(GTK_COMBO_BOX(Background)))
+      gtk_combo_box_set_active(GTK_COMBO_BOX(Background), 2);
 }
 
 /*
@@ -175,7 +202,7 @@ int gui_attach_gtk3_meet4kctrls (GtkWidget *parent)
 
 	int line = 0;
 
-	/* Background Mode */
+	/* Camera effect */
 	line++;
 
 	GtkWidget* label_BackgroundMode = gtk_label_new(_("Virtual Background Mode:"));
@@ -204,7 +231,7 @@ int gui_attach_gtk3_meet4kctrls (GtkWidget *parent)
 		gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT(BackgroundMode),
 										_("Track"));
 
-	uint8_t cur_BackgroundMode = meet4kcore_get_background_mode(get_v4l2_device_handler());
+	uint8_t cur_BackgroundMode = meet4kcore_get_camera_effect(get_v4l2_device_handler());
 	int BackgroundMode_index = cur_BackgroundMode;
 	if(BackgroundMode_index < 0)
 		BackgroundMode_index = 0;
@@ -213,7 +240,7 @@ int gui_attach_gtk3_meet4kctrls (GtkWidget *parent)
 
 	//connect signal
 	g_signal_connect (GTK_COMBO_BOX_TEXT(BackgroundMode), "changed",
-			G_CALLBACK (meet4k_background_mode_changed), NULL);
+			G_CALLBACK (meet4k_camera_effect_changed), NULL);
 
 	gtk_grid_attach (GTK_GRID(meet4k_controls_grid), BackgroundMode, 1, line, 1 ,1);
 	gtk_widget_show (BackgroundMode);
@@ -260,6 +287,84 @@ int gui_attach_gtk3_meet4kctrls (GtkWidget *parent)
 
 	gtk_grid_attach (GTK_GRID(meet4k_controls_grid), CameraAngle, 1, line, 1 ,1);
 	gtk_widget_show (CameraAngle);
+
+	/* Camera bg mode */
+	line++;
+
+	GtkWidget* label_Background = gtk_label_new(_("Background:"));
+#if GTK_VER_AT_LEAST(3,15)
+	gtk_label_set_xalign(GTK_LABEL(label_Background), 1);
+	gtk_label_set_yalign(GTK_LABEL(label_Background), 0.5);
+#else
+	gtk_misc_set_alignment (GTK_MISC (label_Background), 1, 0.5);
+#endif
+	gtk_grid_attach (GTK_GRID(meet4k_controls_grid), label_Background, 0, line, 1, 1);
+	gtk_widget_show (label_Background);
+
+	uint8_t min_Background = 0;
+	uint8_t max_Background = 3;
+
+
+	Background = gtk_combo_box_text_new();
+	if(max_Background >= 1 && min_Background < 2)
+		gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(Background),
+									"\x01", _("Color"));
+	if(max_Background >= 2 && min_Background < 3)
+		gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(Background),
+									"\x11", _("Image"));
+	if(max_Background >= 3 && min_Background < 4)
+		gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(Background),
+									"\x12", _("Blur"));
+
+	char cur_Background[] = { (char)meet4kcore_get_bg_mode(get_v4l2_device_handler()), 0};
+	gtk_combo_box_set_active_id(GTK_COMBO_BOX(Background), cur_Background);
+
+	//connect signal
+	g_signal_connect (GTK_COMBO_BOX_TEXT(Background), "changed",
+			G_CALLBACK (meet4k_background_changed), NULL);
+
+	gtk_grid_attach (GTK_GRID(meet4k_controls_grid), Background, 1, line, 1 ,1);
+	gtk_widget_show (Background);
+
+	/*blur*/
+	line++;
+
+	GtkWidget *label_Blur = gtk_label_new(_("Blur:"));
+#if GTK_VER_AT_LEAST(3,15)
+	gtk_label_set_xalign(GTK_LABEL(label_Blur), 1);
+	gtk_label_set_yalign(GTK_LABEL(label_Blur), 0.5);
+#else
+	gtk_misc_set_alignment (GTK_MISC (label_Blur), 1, 0.5);
+#endif
+
+	gtk_grid_attach (GTK_GRID(meet4k_controls_grid), label_Blur, 0, line, 1, 1);
+	gtk_widget_show (label_Blur);
+
+	uint8_t blur_level = meet4kcore_get_blur_level(get_v4l2_device_handler());
+
+	if(debug_level > 2)
+		printf("GUVCVIEW: blur level is set to %d\n", blur_level);
+
+	GtkAdjustment *blur_adjustment =  gtk_adjustment_new (
+		(double)blur_level,
+		0.0,
+		64.0,
+		1.0,
+		10.0,
+		0.0);
+
+	GtkWidget *blur_scale = gtk_scale_new (GTK_ORIENTATION_HORIZONTAL, blur_adjustment);
+	gtk_scale_set_digits(GTK_SCALE(blur_scale), 3);
+	gtk_scale_set_value_pos(GTK_SCALE(blur_scale), GTK_POS_RIGHT);
+	gtk_widget_set_halign (blur_scale, GTK_ALIGN_FILL);
+	gtk_widget_set_hexpand (blur_scale, TRUE);
+	gtk_grid_attach(GTK_GRID(meet4k_controls_grid), blur_scale, 1, line, 1, 1);
+	gtk_widget_show (blur_scale);
+
+	gtk_widget_set_sensitive (blur_scale, TRUE);
+
+	g_signal_connect (GTK_SCALE(blur_scale), "value-changed",
+		G_CALLBACK (meet4k_blur_level_changed), NULL);
 
 	/* modes grid*/
 	line++;
